7fcf76049b6ca0146020e9c96e85e32967e6ab29
Piyush Katira <piyush@katirap-a01.vmware.com>
2022-03-31 19:45:59 +0400

    initial commit

---
+++ polls/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.contrib import admin
+from django.urls import include, path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls)	
+]
+
